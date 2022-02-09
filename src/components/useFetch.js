83794eb0ec8d77cf093bce7ab8d5f4e01950225f
federico-palma <fedepal9@gmail.com>
2022-02-09 13:46:33 +0100

    Imported useFetch Hook from another project

---
+++ src/components/useFetch.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+    
+    useEffect(() => {
+        const abortCtr = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCtr.signal})
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Resource could not be fetched');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('Fetch has been aborted')
+                } else {
+                    setError(err.message);
+                    setIsPending(null);
+                }
+            });
+        }, 1000);
+
+        return () => abortCtr.abort();
+    }, [url]);
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
+
