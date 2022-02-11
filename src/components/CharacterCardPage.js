4935a9acfff271d39abdfe44118a6f850d7ea3c7
federico-palma <fedepal9@gmail.com>
2022-02-11 10:47:39 +0100

    Added border color on img based on gender

--- src/components/CharacterCardPage.js
+++ src/components/CharacterCardPage.js
@@ -2,11 +2,31 @@ const CharacterCardPage = (charData) => {
     // For some reason data comes as a JSobject with array as only property, here's destructered into an array.
     const { charData: dataArray } = charData
 
+    function setImgGenderColor(charGender) {
+        switch (charGender) {
+            case 'Male':
+                return '0px 0px 5px 3px #1512da'
+                break;
+            case 'Female':
+                return '0px 0px 5px 3px #da122d'
+                break;
+            case 'Genderless':
+                return '0px 0px 5px 3px #ee7b22'
+                break;
+            case 'unknown':
+                return '0px 0px 5px 3px #4ecf27'
+                break;
+            default:
+                return '0px 0px 5px 3px #f60fe3'
+                break;
+        }
+    }
+
     return (
         <div className="character-card-page">
             { dataArray.map((singleCharData) => (
                 <div className="char-card" key={ singleCharData.id }>
-                    <img src={singleCharData.image} alt="" className="char-img"/>
+                    <img src={singleCharData.image} alt="" className="char-img" style={{boxShadow: setImgGenderColor(singleCharData.gender)}}/>
                     <h2 className="char-name">{ singleCharData.name }</h2>
                     <p className="char-id">{ singleCharData.id }</p>
                 </div>
