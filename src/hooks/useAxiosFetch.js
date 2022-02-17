a4f3ba1fa1fd66f6438f536f644574d3c1b0f833
federico-palma <fedepal9@gmail.com>
2022-02-17 13:12:06 +0100

    Set timeout for testing

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -6,14 +6,14 @@ function useAxiosFetch(endpoint, pageNumber, query) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [hasMore, setHasMore ] = useState(true);
-    const [nextPageUrl, setNextPageUrl] = useState();
-
 
     useEffect(() => {
         setLoading(true);
         setError(false);
-
         let cancel;
+
+        setTimeout(() => {
+            
         axios({
             method: 'GET',
             baseURL:'https://rickandmortyapi.com/api/',
@@ -30,16 +30,16 @@ function useAxiosFetch(endpoint, pageNumber, query) {
             })
             setLoading(false)
             setHasMore(res.data.info.next !== null)
-            setNextPageUrl(res.data.info.next)
         }).catch(e => {
             setError(true)
             if (axios.isCancel(e)) return
         })
 
         return  () => cancel;
+        }, 2000);
     }, [query, pageNumber])
 
-    return { data, loading, error, hasMore, nextPageUrl }
+    return { data, loading, error, hasMore }
 }
 
 export default useAxiosFetch;
