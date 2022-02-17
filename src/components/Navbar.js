4fd981995ef20ac17d5912d4f15eaa07d02cde7d
federico-palma <fedepal9@gmail.com>
2022-02-17 13:12:28 +0100

    Added hamburger menu on resize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [isHamMenuOpen, setIsHamMenuOpen] = useState(false)
+
+    function toggleHamburger() {
+        setIsHamMenuOpen(!isHamMenuOpen);
+    }
+
     return (
         <nav className="navbar">
             <Link to='/'>
                 <img id="logo" src={require("../images/RickAndMortyLogo.png")} alt="Rick and Morty logo"/>
             </Link>
-            <div className="navbar-links">
+            <div id="hamburger" onClick={ toggleHamburger } className={ isHamMenuOpen ? 'open-burger' : null }>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+            </div>
+            <div className={ isHamMenuOpen ? 'navbar-links open-menu' : "navbar-links closed-menu" }>
                 <Link to='/characters'>
                     <p id="nav-link-character">Characters</p>
                 </Link>
